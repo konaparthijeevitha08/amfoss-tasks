7f2e7aa203b931b8a85bb18dff2bd8758396047a
CAPTAIN08 <91887848+konaparthijeevitha08@users.noreply.github.com>
2021-11-21 08:10:04 +0530

    Delete TASK #10/pset directory

--- TASK #10/pset/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h> //include libraries
-
-int main(void)
-{
-    string name = get_string("What is your name?\n");//get user input
-    printf("Hello, %s", name); //print hello, user input
-}
